d8df334d83bf78e6f15addbbdeeb44744e1c7457
yolocap <dhaneshwaran24@gmail.com>
2025-04-21 07:55:01 +0530

    java files creating

---
+++ and_op.java
@@ -0,0 +1,15 @@
+class and_op{
+    public static void main(String[] args) {
+        boolean hungry = false;
+        boolean icecream = true;
+        if (icecream && hungry) {
+            System.out.println("I will eat icecream");
+            
+            
+        } else {
+            System.out.println(" Don't Eat");
+        }
+            
+        }
+    
+}
