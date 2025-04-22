cf5ebd14baec8038591cc955b820d8e2b72abe78
yolocap <dhaneshwaran24@gmail.com>
2025-04-22 17:04:20 +0530

    modified

--- q19.java
+++ q19.java
@@ -1,3 +1,9 @@
 public class q19 {
-    
+    public static void main(String[] args) {
+        int[] num = { 0, 1, 2, 3, 4, 5, 6, 7, 8, 9 };
+        for (int i = 0; i <= 9; i++) {
+            System.out.println(num[i]);
+        }
+    }
+
 }
