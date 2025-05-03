6fb7125fad7c59dfb781976ef14e9d0ad2aab874
yolocap <dhaneshwaran24@gmail.com>
2025-05-03 09:16:45 +0530

    inheritance file added

---
+++ inheri.java
@@ -0,0 +1,17 @@
+class dad {
+	int money =100000;
+}
+// using inheritance keyword "extends"
+class son extends dad {
+	
+}
+
+public class inheri {
+
+	public static void main(String[] args) {
+		son s1 = new son();
+		System.out.println(s1.money);
+
+	}
+
+}
